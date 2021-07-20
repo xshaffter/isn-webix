eeb6f1cf5c12066e7418b974985748485d2479d9
Alfredo Martinez <amartinez@isnstudio.com>
2021-07-20 17:14:08 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+setup(
+    name='isn-webix',
+    version='0.1.0',
+    packages=['isn_webix'],
+    description='Work with prepared Webix-django classes',
+    long_description=README,
+    author='Alfredo Martinez',
+    author_email='xshaffter@gamil.com',
+    url='https://github.com/xshaffter/django-isn-webix/',
+    license='MIT',
+    install_requires=[
+        'Django>=1.11.29',
+        'djangorestframework>=3.9.4'
+        'api-auto-doc>=0.1.0'
+    ]
+)
